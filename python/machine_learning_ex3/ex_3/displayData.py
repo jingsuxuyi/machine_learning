1620efdfca14ec8f85ddfe9bb41621f492587c1c
raylei <jingsuxuyi@163.com>
2018-12-23 19:18:31 +0800

    finish the part1 of ex3

---
+++ python/machine_learning_ex3/ex_3/displayData.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 22 21:48:24 2018
+
+@author: Jingjing
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def displayData(x, examle_width=0):
+#DISPLAYDATA Display 2D data in a nice grid
+#   [h, display_array] = DISPLAYDATA(X, example_width) displays 2D data
+#   stored in X in a nice grid. It returns the figure handle h and the 
+#   displayed array if requested.
+    m, n = x.shape
+    # Set example_width automatically if not passed in
+    if examle_width == 0:
+        example_width = np.round(np.sqrt(n)).astype(np.int32)
+    example_height = (n / example_width).astype(np.int32)
+    
+    #  Compute number of items to display
+    display_rows = np.floor(np.sqrt(m)).astype(np.int32)
+    display_cols = np.ceil(m / display_rows).astype(np.int32)
+    
+    # Between images padding
+    pad = 1
+
+    # Setup blank display
+    display_array = - np.ones((pad + display_rows * (example_height + pad),
+                              pad + display_cols * (example_height + pad)))  
+    
+    # Copy each example into a patch on the display array
+    curr_ex = 0
+    for j in np.arange(display_rows):
+        for i in np.arange(display_rows):
+            if curr_ex > m:
+                break
+            #copy the patch
+            
+            # get the max values of the patch
+            max_val = np.max(x[curr_ex, :])
+            display_array[pad+j*(example_height+pad)+np.arange(example_height),\
+                          pad+i*(example_width+pad)+np.arange(example_width)[:,np.newaxis]]=\
+                          x[curr_ex].reshape((example_height, example_width)) / max_val
+                       
+            #print(pad+i*(example_width+pad)+np.arange(example_width))
+            curr_ex += 1
+            if curr_ex > m:
+                break
+            
+    # Display image
+    plt.figure()
+    plt.imshow(display_array, cmap='gray', extent=[-1, 1, -1, 1])
+    plt.axis('off') 
+    
--- python/machine_learning_ex3/ex_3/ex_3.py
+++ python/machine_learning_ex3/ex_3/ex_3.py
@@ -5,3 +5,48 @@ Created on Sat Dec 22 11:17:20 2018
 @author: Jingjing
 """
 
+## Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
+
+#  Instructions
+#  ------------
+#
+#  This file contains code that helps you get started on the
+#  linear exercise. You will need to complete the following functions
+#  in this exericse:
+#
+#     lrCostFunction.py (logistic regression cost function)
+#     oneVsAll.py
+#     predictOneVsAll.py
+#     predict.py
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+#
+import numpy as np
+import scipy.io as sio
+import displayData
+
+# Setup the parameters you will use for this part of the exercise
+input_layer_size  = 400             # 20x20 Input Images of Digits
+num_labels = 10                     # 10 labels, from 1 to 10 (note that we have mapped "0" to label 10)
+
+## =========== Part 1: Loading and Visualizing Data =============
+#  We start the exercise by first loading and visualizing the dataset.
+#  You will be working with a dataset that contains handwritten digits.
+#
+
+# Load Training Data
+print("Loading and Visualizing Data ...")
+
+mat_contents = sio.loadmat("ex3data1.mat")
+
+X, y = mat_contents['X'], mat_contents['y']
+
+m = y.size
+
+# Randomly select 100 data points to display
+
+rand_indices = np.random.randint(0, m, size=100)
+sel = X[rand_indices, :]
+
+displayData.displayData(sel)
